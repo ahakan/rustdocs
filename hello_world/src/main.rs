86c23b32d28673281d1d8d627f7bfa011e2ab7b3
Ahmet Hakan Çelik <ahmethcelik@gmail.com>
2022-02-22 12:40:57 +0300

    hello_world

---
+++ hello_world/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = 5;
+
+    let mut y = 10;     // mutable variable
+
+    println!("Hello, world! x: {}", x);
+
+    // x = x + 5;       // immutable
+    y = y + 5;
+
+    println!("y: {}", y);
+
+    let z: u64 = 10;    // i->signed integer u->unsigned integer
+
+    println!("unsigned64 z: {}", z);
+
+}
